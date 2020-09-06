6d27e8880f8efdb588098f7572b6d9b71679bb90
frontseat-astronaut <frontseatastronaut@gmail.com>
2020-09-06 15:16:29 +0530

    added test

---
+++ test/test.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+#include <node.h>
+#include <operations.h>
+#include <variables.h>
+#include <op_api.h>
+#include <var_api.h>
+
+using namespace dio;
+
+int main()
+{
+    Node a = IndependentVariable("a");
+    assign(a, 2);
+    Node b = Constant(6, "b");
+    printf("a: %lf\n", value(a)); 
+    printf("b: %lf\n", value(b)); 
+
+    Node c = Add(Multiply(a, b), Reciprocal(a));
+    printf("c: %lf\n", value(b)); 
+    printf("c key (prefix notation!): %s\n", key(c).c_str()); 
+    printf("del c / del a: %lf\n", gradient(c, a)); 
+
+}
